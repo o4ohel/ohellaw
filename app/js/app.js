0a0744063814653b76e4f40735e53a42fbe264a8
Ofer Ohel <ofer.ohel@totvs.com>
2014-11-09 20:20:21 -0800

    First go at ohellaw redesign using foundation

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,50 @@
-var app = (function(document, $) {
-
-	'use strict';
-	var docElem = document.documentElement,
-
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-			_userAgentInit();
-		};
+var app = (function(document, $) {'use strict';
+	var docElem = document.documentElement, _userAgentInit = function() {
+		docElem.setAttribute('data-useragent', navigator.userAgent);
+	}, _init = function() {
+		$(document).foundation();
+		_userAgentInit();
+	};
 
 	return {
-		init: _init
+		init : _init
 	};
 
 })(document, jQuery);
 
-(function() {
-
-	'use strict';
+(function() {'use strict';
 	app.init();
 
 })();
+
+angular.module('app', ['ngRoute']).config(function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/home', {templateUrl : 'views/home.html', controller : 'HomeController'})
+	.when('/areas', {templateUrl : 'views/areas.html', controller : 'AreasController'})
+	.when('/profile', {templateUrl : 'views/profile.html', controller : 'ProfileController'})
+	.when('/testimonials', {templateUrl : 'views/testimonials.html', controller : 'TestimonialsController'})
+	.when('/contact', {templateUrl : 'views/contact.html', controller : 'ContactController'})
+	.when('/estateplan', {templateUrl: 'views/estateplan.html', controller: 'EstateplanController'})
+	.when('/disclaimer', {templateUrl : 'views/disclaimer.html'})
+	.otherwise({redirectTo : '/home'});
+
+	// configure html5 to get links working on jsfiddle
+	// $locationProvider.html5Mode(true);
+}).controller('MainController', ['$scope', '$route', '$routeParams', '$location',function($scope, $route, $routeParams, $location) {
+	$scope.$route = $route;
+	$scope.$location = $location;
+	$scope.$routeParams = $routeParams;
+}]).controller('HomeController', ['$scope', '$http',function($scope, $http) {
+	$http.get('../data/testimonials.json').success(function(data) {
+		$scope.testimonials = data.items;
+	});
+}]).controller('AreasController', ['$scope',function($scope) {
+
+}]).controller('ProfileController', ['$scope',function($scope) {
+
+}]).controller('TestimonialsController', ['$scope',function($scope) {
+
+}]).controller('ContactController', ['$scope',function($scope) {
+
+}])
+; 
