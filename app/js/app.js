e22f0b1f2faac4c063fa28a3fd5c3ae775caf47c
Ofer Ohel <ofer.ohel@totvs.com>
2014-11-09 23:36:21 -0800

    changed some configs so that 'grunt publish' will not throw error for angular not defined

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,6 @@
-var app = (function(document, $) {'use strict';
+'use strict';
+
+var app = (function(document, $) {
 	var docElem = document.documentElement, _userAgentInit = function() {
 		docElem.setAttribute('data-useragent', navigator.userAgent);
 	}, _init = function() {
@@ -12,31 +14,29 @@ var app = (function(document, $) {'use strict';
 
 })(document, jQuery);
 
-(function() {'use strict';
+(function() {
 	app.init();
-
 })();
 
-angular.module('app', ['ngRoute']).config(function($routeProvider, $locationProvider) {
+angular.module('app', ['ngRoute']).config(function($routeProvider) {
 	$routeProvider
 	.when('/home', {templateUrl : 'views/home.html', controller : 'HomeController'})
 	.when('/areas', {templateUrl : 'views/areas.html', controller : 'AreasController'})
-	.when('/profile', {templateUrl : 'views/profile.html', controller : 'ProfileController'})
+	.when('/profile', {templateUrl : 'views/profile.html'})
 	.when('/testimonials', {templateUrl : 'views/testimonials.html', controller : 'TestimonialsController'})
 	.when('/contact', {templateUrl : 'views/contact.html', controller : 'ContactController'})
 	.when('/estateplan', {templateUrl: 'views/estateplan.html', controller: 'EstateplanController'})
 	.when('/disclaimer', {templateUrl : 'views/disclaimer.html'})
 	.otherwise({redirectTo : '/home'});
-
-	// configure html5 to get links working on jsfiddle
-	// $locationProvider.html5Mode(true);
 }).controller('MainController', ['$scope', '$route', '$routeParams', '$location',function($scope, $route, $routeParams, $location) {
 	$scope.$route = $route;
 	$scope.$location = $location;
 	$scope.$routeParams = $routeParams;
 	$scope.randomizeList = function(list) {
 		var i = list.length;
-  		if(i == 0) return false;
+  		if(i === 0) {
+  			return false;
+  		}
   		while(--i) {
     		var j = Math.floor(Math.random() * (i + 1)),
     			tempi = list[i],
@@ -51,14 +51,12 @@ angular.module('app', ['ngRoute']).config(function($routeProvider, $locationProv
 		$scope.testimonials = data.items;
 	});
 }]).controller('AreasController', ['$scope',function($scope) {
-
-}]).controller('ProfileController', ['$scope',function($scope) {
-
+	$scope.foo = 'bar';
 }]).controller('TestimonialsController', ['$scope', '$http',function($scope, $http) {
 	$http.get('data/testimonials.json').success(function(data) {
 		$scope.testimonials = $scope.randomizeList(data.items);
 	});
 }]).controller('ContactController', ['$scope',function($scope) {
-
+	$scope.foo = 'bar';
 }])
 ; 
