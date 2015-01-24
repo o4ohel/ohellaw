a841d5f530a1023153915227b5fca8ddfd1ffad5
Ofer Ohel <ofer.ohel@primarydata.com>
2015-01-23 22:41:58 -0800

    - add the data folder to dist build  - dynamically change the container class to container-fluid for the estateplan view.  - start breaking up the estateplan view into smaller chuncks (included via ng-include)  - updated npm and reinstalled image gif package (was throwing error)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,11 @@
 angular.module('ohellawApp')
   .controller('MainCtrl', function ($scope, $rootScope, $state) {
   	$rootScope.$state = $state;
+
+  	$rootScope.$on('$stateChangeStart', function(event, toState){
+  		$rootScope.fluidBodyLayout = (toState.name === 'estateplan');
+	});
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
