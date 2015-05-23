ac7d42ed0aa273e417aa6963bc8c634f759341f9
Ofer Ohel <ofer.ohel@primarydata.com>
2015-05-23 14:00:15 -0700

    Enhanced the googleplaces directive so that it does NOT submit the form when using arrow up/down and enter to select an address.

--- app/scripts/directives/googleplace.js
+++ app/scripts/directives/googleplace.js
@@ -9,8 +9,15 @@ angular.module('ohellawApp').directive('googleplace', function() {
                 componentRestrictions: {}
             };
             scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
- 
+
+            google.maps.event.addDomListener(element[0], 'keydown', function(e) { 
+                if (e.keyCode === 13) { 
+                    e.preventDefault(); 
+                }
+            }); 
+
             google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                // console.log(arguments);
                 scope.$apply(function() {
                     model.$setViewValue(element.val());                
                 });
